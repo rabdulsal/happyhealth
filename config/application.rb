2c4ae2388d90c378a03602e83f0c27170ae72807
Rashad Abdul-Salaam <rashad@getskinclick.com>
2013-03-31 07:09:43 -0500

    Pre-comile fix, again

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module HappyHealth
     config.active_record.whitelist_attributes = true
 
     # Precompile Fix
-    #config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
