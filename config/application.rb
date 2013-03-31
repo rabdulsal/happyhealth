6b6b1f386bd7ea87ae6a595f73af9dbe8aeb65cb
Rashad Abdul-Salaam <rashad@getskinclick.com>
2013-03-30 22:33:11 -0500

    Asset initialize_on_precompile => false addition

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module HappyHealth
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    # Precompile Fix
+    config.assets.initialize_on_precompile = false
+    
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.debug = false
