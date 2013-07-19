baebf014ad04ab0fbdd59c9f34dc64c40698e126
Rashad Abdul-Salaam <rashad@getskinclick.com>
2013-07-19 13:24:40 -0500

    Changes to Notes and Sign_in UI, plus Notes Seed in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -207,29 +207,18 @@ class ApplicationController < ActionController::Base
       )
 
     #Test Notes
-    user = User.find(1)
+    user = User.find(1)    
+    user.notes.destroy_all
 
-    if user.notes.nil?
-      user.notes[0].create(
-        info: "Problem - Medication side-effect: I feel nauseous. don't want to think about food."
-        )
-      user.notes[1].create(
-        info: "Update - Still no real appetite after a few days, particularly in the mornings. also feeling dizzy lately."
-        )
-      user.notes[2].create(
-        info: "Reminder - Ask doctor about adjusting the medication, or other alternatives due to these strong side-effects."
-        )
-    else
-      user.notes[0].update_attributes(
-        info: "Problem - Medication side-effect: I feel nauseous. don't want to think about food."
-        )
-      user.notes[1].update_attributes(
-        info: "Update - Still no real appetite after a few days, particularly in the mornings. also feeling dizzy lately."
-        )
-      user.notes[2].update_attributes(
-        info: "Reminder - Ask doctor about adjusting the medication, or other alternatives due to these strong side-effects."
-        )
-    end
+    user.notes.create(
+      info: "Problem - Medication side-effect: I feel nauseous. don't want to think about food."
+      )
+    user.notes.create(
+      info: "Update - Still no real appetite after a few days, particularly in the mornings. also feeling dizzy lately."
+      )
+    user.notes.create(
+      info: "Reminder - Ask doctor about adjusting the medication, or other alternatives due to these strong side-effects."
+      )
 
     if current_user.form
       user_form_path(current_user.id, current_user.form.id)
